a51b1eb4377f6647d529bf0b55f6fb511deb6e20
Ankur <anx450z@gmail.com>
2023-03-24 23:59:24 +0530

    user create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.errors.empty?
+      render json: { status: "successfully created new user" }
+    else
+      render json: { error: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
