36e0910784caee0ebbe8f52a4b06c27931d30810
Ankur <anx450z@gmail.com>
2023-03-25 21:47:25 +0530

    updated show action for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    collection  do
+      get :show
+    end
+  end
+
 end
